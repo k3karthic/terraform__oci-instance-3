250b1f7bd093d4f80e25338e6a30ca927e88582b
Karthic Kumaran <k3.karthic@protonmail.ch>
2022-01-22 13:02:22 +0000

    open port 9999

--- main.tf
+++ main.tf
@@ -113,3 +113,20 @@ resource "oci_core_network_security_group_security_rule" "ssh" {
     }
   }
 }
+
+resource "oci_core_network_security_group_security_rule" "projector" {
+  network_security_group_id = oci_core_network_security_group.free.id
+
+  direction = "INGRESS"
+  protocol  = 6 # TCP
+
+  source      = "0.0.0.0/0"
+  source_type = "CIDR_BLOCK"
+
+  tcp_options {
+    destination_port_range {
+      min = 9999
+      max = 9999
+    }
+  }
+}
